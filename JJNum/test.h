91d472955fb0e87156d09b319cfd2e401c83bd08
ZJ <zj@ZJdeMacBook-Pro.local>
2018-12-07 15:09:45 +0800

    提交

---
+++ JJNum/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  const
+//
+//  Created by ZJ on 2018/1/31.
+//  Copyright © 2018年 ZJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
++ (void)test;
+@end
---
+++ JJNum/test.m
@@ -0,0 +1,16 @@
+//
+//  test.m
+//  const
+//
+//  Created by ZJ on 2018/1/31.
+//  Copyright © 2018年 ZJ. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
++ (void)test
+{
+    NSLog(@"提交试用成功");
+}
+@end
